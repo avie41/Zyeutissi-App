946a80bd5646796a0bc98f699d8207c8edaccdd9
Alexis VIE <alexis.vie@etu.univ-nantes.fr>
2022-02-19 18:28:05 +0100

    adding translations to switchmode page

--- App.tsx
+++ App.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import './config/firebase';
 import { DataProvider } from './src/hooks';
 import RootNavigation from './src/navigation/App';
-import {AsyncStorage} from '@react-native-async-storage/async-storage'
 
 
 
--- src/screens/SwitchMode.tsx
+++ src/screens/SwitchMode.tsx
@@ -1,101 +1,97 @@
+import { Platform } from 'react-native';
+import { useNavigation } from '@react-navigation/core';
 
-import React, {useCallback} from 'react';
-import {Platform, Linking} from 'react-native';
-import {Ionicons} from '@expo/vector-icons';
-import {useNavigation} from '@react-navigation/core';
-
-import {Block, Button, Image, Text} from '../components/';
-import {useData, useTheme, useTranslation} from '../hooks/';
+import { Block, Button, Image, Text } from '../components/';
+import { useData, useTheme, useTranslation } from '../hooks/';
 
 const isAndroid = Platform.OS === 'android';
 
 
 const SwitchMode = () => {
-  const {user} = useData();
-  const {t} = useTranslation();
+  const { t } = useTranslation();
   const navigation = useNavigation();
-  const {assets, colors, sizes} = useTheme();
-  
+  const { assets, colors, sizes } = useTheme();
+
   return (
     <Block safe marginTop={sizes.md}>
       <Block
-          scroll
-          paddingHorizontal={sizes.padding}
-          showsVerticalScrollIndicator={false}
-          contentContainerStyle={{paddingBottom: sizes.padding}}>
-        <Block flex={0}>  
+        scroll
+        paddingHorizontal={sizes.padding}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: sizes.padding }}>
+        <Block flex={0}>
           <Block align='center'>
-          <Image
+            <Image
               width={240}
               height={240}
               source={assets.zyeutissi}
             />
-            <Text h4 marginBottom={sizes.s} marginTop={sizes.sm} color = {colors.blue} align = 'center'>
-                Je souhaite...
+            <Text h4 marginBottom={sizes.s} marginTop={sizes.sm} color={colors.blue} align='center'>
+              Je souhaite...
             </Text>
           </Block>
 
-          
+
           <Block
             flex={0}
             marginTop={sizes.xxl}
             align='center'
-          >            
-          <Button              
+          >
+            <Button
               flex={0}
               color={colors.card}
               padding={sizes.padding}
               width={270}
               onPress={() => navigation.navigate("Home")}>
-              <Image              
+              <Image
                 width={100}
-                height={80}              
+                height={80}
                 source={assets.zyeuter}
-                style={{position: 'relative', top: -60}}
-                marginBottom={-60}                        
+                style={{ position: 'relative', top: -60 }}
+                marginBottom={-60}
               />
-              <Text h4 color = {colors.blue} align='center' marginBottom={5}>
-                Zyeuter         
+              <Text h4 color={colors.blue} align='center' marginBottom={5}>
+                {t('navigation.zyeuter')}
               </Text>
-              <Text h5 color = {colors.blue}>
-                Visionner nos vidéos
+              <Text h5 color={colors.blue}>
+                {t('navigation.zyeuterDetails')}
               </Text>
 
-          </Button>
+            </Button>
           </Block>
 
           <Block
             flex={0}
             marginTop={sizes.xxl}
             align='center'
-          >            
-          <Button              
+          >
+            <Button
               flex={0}
               color={colors.card}
               padding={sizes.padding}
               width={270}
               onPress={() => navigation.navigate("ToDevelop")}>
-              <Image              
+              <Image
                 width={100}
-                height={80}              
+                height={80}
                 source={assets.transmettre}
-                style={{position: 'relative', top: -60}}
-                marginBottom={-60}                        
+                style={{ position: 'relative', top: -60 }}
+                marginBottom={-60}
               />
-              <Text h4 color = {colors.blue} align='center' marginBottom={5}>
-                Transmettre         
+              <Text h4 color={colors.blue} align='center' marginBottom={5}>
+                {t('navigation.transmettre')}
               </Text>
-              <Text h5 color = {colors.blue}>
-                Exposer vos vidéos
+              <Text h5 color={colors.blue}>
+                {t('navigation.transmettreDetails')}
               </Text>
 
-          </Button>
+            </Button>
           </Block>
         </Block>
       </Block>
     </Block>
-    );
-  };
-  
-  export default SwitchMode;
-  
+  );
+};
+
+export default SwitchMode;
+
